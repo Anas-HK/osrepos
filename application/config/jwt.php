a3fcfc758e43ad557c2970421e3c569919855e07
Anas Khan <anas.h.k2244@gmail.com>
2024-03-08 17:48:52 +0500

    Testing

--- application/config/jwt.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$random_bytes = random_bytes(32);
-$secret_key = base64_encode($random_bytes);
-
-$config['jwt_key'] = $secret_key;
-?>
